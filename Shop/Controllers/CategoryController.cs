27176f26163d7258e43c583c494d16c56831f7a5
Kauan <kauanhindlmayer07@gmail.com>
2022-11-10 09:19:16 -0300

    Capturando o JSON enviado

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,18 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
-    // Endpoint => URL
-    // https://localhost:5001/categories
-
     [Route("categories")]
     public class CategoryController : ControllerBase
     {
+        // Endpoint => URL
         // https://localhost:5001/categories
+
+        [HttpGet]
         [Route("")]
-        public string MeuMetodo()
+        public string Get()
         {
-            return "Olá, Mundo!";
+            return "GET";
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public string GetById(int id)
+        {
+            return "GET " + id.ToString();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public Category Post([FromBody]Category model)
+        {
+            return model;
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "PUT";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
+        {
+            return "DELETE";
         }
     }
 }
