3bcf4ae10cc00a978c01ebfe74b08d7cb0eac2fb
Kauan <kauanhindlmayer07@gmail.com>
2022-11-10 08:48:50 -0300

    Initial commit

---
+++ Shop/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    // Endpoint => URL
+    // https://localhost:5001/categories
+
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        // https://localhost:5001/categories
+        [Route("")]
+        public string MeuMetodo()
+        {
+            return "Olá, Mundo!";
+        }
+    }
+}
