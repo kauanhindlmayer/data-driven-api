de34c5935cfc5b8c340317cebecf96f49f15463a
Kauan <kauanhindlmayer07@gmail.com>
2022-11-10 10:37:32 -0300

    DataContext

---
+++ Shop/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
