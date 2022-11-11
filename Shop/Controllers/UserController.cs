886c714a2db77355bdb86ff26263eda0141c1d05
Kauan <kauanhindlmayer07@gmail.com>
2022-11-11 11:56:17 -0300

    Autenticando

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post (
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            var user = await context.Users
+                .AsNoTracking()
+                .Where(x => x.Username == model.Username && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
